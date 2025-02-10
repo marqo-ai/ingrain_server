0b79616c720703cfe3e4e95113379b15daf8e4ca
Li Wan <li@marqo.ai>
2025-02-10 12:51:57 +1100

    Add tensorrt optimization

--- inference/triton_open_clip/clip_converting.py
+++ inference/triton_open_clip/clip_converting.py
@@ -64,13 +64,6 @@ instance_group [
         gpus: [0]
     }}
 ]
-optimization {{ execution_accelerators {{
-  gpu_execution_accelerator : [ {{
-    name : "tensorrt"
-    parameters {{ key: "precision_mode" value: "FP16" }}
-    parameters {{ key: "max_workspace_size_bytes" value: "1073741824" }}
-    }}]
-}}
 input [
   {{
     name: "input"
@@ -87,7 +80,15 @@ output [
 ]
 dynamic_batching {{
     max_queue_delay_microseconds: {max_queue_delay_microseconds}
-}}"""
+}}
+optimization {{ execution_accelerators {{
+  gpu_execution_accelerator : [ {{
+    name : "tensorrt"
+    parameters {{ key: "precision_mode" value: "FP16" }}
+    parameters {{ key: "max_workspace_size_bytes" value: "1073741824" }}
+    }}]
+}}
+"""
     with open(os.path.join(cfg_path, "config.pbtxt"), "w") as f:
         f.write(config)
 
