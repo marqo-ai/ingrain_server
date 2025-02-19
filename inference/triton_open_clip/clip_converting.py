279cc50fcc62ae7d88243de94b5673aa17ef574e
Li Wan <li@marqo.ai>
2025-02-19 11:24:51 +1100

    Remove tensorrt

--- inference/triton_open_clip/clip_converting.py
+++ inference/triton_open_clip/clip_converting.py
@@ -81,14 +81,6 @@ output [
 dynamic_batching {{
     max_queue_delay_microseconds: {max_queue_delay_microseconds}
 }}
-optimization {{ execution_accelerators {{
-  gpu_execution_accelerator : [ {{
-    name : "tensorrt"
-    parameters {{ key: "precision_mode" value: "FP16" }}
-    parameters {{ key: "max_workspace_size_bytes" value: "1073741824" }}
-    }}]
-}}
-}}
 """
     with open(os.path.join(cfg_path, "config.pbtxt"), "w") as f:
         f.write(config)
@@ -131,14 +123,6 @@ output [
 ]
 dynamic_batching {{
     max_queue_delay_microseconds: {max_queue_delay_microseconds}
-}}
-optimization {{ execution_accelerators {{
-  gpu_execution_accelerator : [ {{
-    name : "tensorrt"
-    parameters {{ key: "precision_mode" value: "FP16" }}
-    parameters {{ key: "max_workspace_size_bytes" value: "1073741824" }}
-    }}]
-}}
 }}
 """
     with open(os.path.join(cfg_path, "config.pbtxt"), "w") as f:
